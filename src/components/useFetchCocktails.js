4b9d69b0ea34fd2ee78f7e48fd5f9bbe0efab318
Preeti <preetijav@gmail.com>
2023-05-02 09:17:34 +0200

    Added custom hook and contextApi

---
+++ src/components/useFetchCocktails.js
@@ -0,0 +1,64 @@
+import { useState, useEffect, createContext } from "react";
+
+export const CocktailsContext = createContext([]);
+
+const useFetchCocktails = () => {
+  const [cocktails, setCocktails] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchCocktails = async () => {
+    setLoading(true);
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+
+      console.log(result)
+
+      const { drinks } = result;
+      if (!drinks) {
+        setCocktails([]);
+      } else {
+        const newCocktails = drinks.map((drink) => {
+          const { idDrink, strDrink, strDrinkThumb, strInstructions, strCategory, strAlcoholic, strGlass } =
+            drink;
+
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            instructions: strInstructions,
+            category: strCategory,
+            info: strAlcoholic,
+            glass: strGlass,
+          };
+        });
+
+        setCocktails(newCocktails);
+      }
+
+      setLoading(false);
+    } catch (error) {
+      setError(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCocktails();
+  }, []);
+
+  return { cocktails, loading, error, fetchCocktails };
+};
+
+export const CocktailProvider = ({ children }) => {
+  const cocktailsContextValue = useFetchCocktails();
+
+  return (
+    <CocktailsContext.Provider value={cocktailsContextValue}>
+      {children}
+    </CocktailsContext.Provider>
+  )
+}
