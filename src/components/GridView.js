e498ec4595d8a347b15bee376eabdf75738a81d4
Preeti <preetijav@gmail.com>
2023-05-02 18:36:16 +0200

    format change

--- src/components/GridView.js
+++ src/components/GridView.js
@@ -5,16 +5,25 @@ import { CocktailsContext } from "./CocktailsContext";
 const GridView = () => {
   const { cocktails } = useContext(CocktailsContext);
 
-
-  return <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} >
-  {cocktails.map((cocktail) => (
-   <Grid item xs={2} sm={4} md={4} key={cocktail.id}>
-      <img src={cocktail.image} alt={cocktail.name}  height="auto" width="100%"/>
-      <div>{cocktail.name}</div>
-     
+  return (
+    <Grid
+      container
+      spacing={{ xs: 2, md: 3 }}
+      columns={{ xs: 4, sm: 8, md: 12 }}
+    >
+      {cocktails.map((cocktail) => (
+        <Grid item xs={2} sm={4} md={4} key={cocktail.id}>
+          <img
+            src={cocktail.image}
+            alt={cocktail.name}
+            height="auto"
+            width="100%"
+          />
+          <div>{cocktail.name}</div>
+        </Grid>
+      ))}
     </Grid>
-  ))}
-</Grid>;
+  );
 };
 
 export default GridView;
