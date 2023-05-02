c28392caf591dfa7bdc411e93d61608d1fc7a032
Preeti <preetijav@gmail.com>
2023-05-02 15:40:41 +0200

    added theme to the app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import { CocktailProvider } from "./components/useFetchCocktails";
-import CocktailList from "./components/CocktailList";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Home from "./pages/Home";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
   return (
     <>
       <CocktailProvider>
-        <CocktailList />
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Container>
+            <Box>
+              <Home />
+            </Box>
+          </Container>
+        </ThemeProvider>
       </CocktailProvider>
     </>
   );
